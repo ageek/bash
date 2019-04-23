476e6c3b9983709676255b12782843f7df6d3155
Ahmed <re.90@live.com>
2019-04-23 22:25:39 +0530

    bash colorized output - tricky script

---
+++ tricks/color-output.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+printf -- 'doing something... \n';
+printf -- '\033[37m someone elses output \033[0m\n';
+printf -- '\033[32m SUCCESS: yay \033[0m\n';
+printf -- '\033[33m WARNING: hmm \033[0m\n';
+printf -- '\033[31m ERROR: fubar \033[0m\n';
+echo bye
