da3331cc03b893ccbb0ea0b769e1d9eec991a7d2
Ahmed <re.90@live.com>
2019-04-24 07:37:27 +0530

    colored bg and fg text in bash

---
+++ tricks/colored-bg-fg.sh
@@ -0,0 +1,8 @@
+echo -e '\E[42;31m'"Red on green"
+tput sgr0
+echo -e '\E[43;30m'"Black on yellow"
+tput sgr0
+echo -e '\E[45;34m'"Blue on magenta"
+tput sgr0
+echo -e '\E[47;36m'"Cyan on white"
+tput sgr0
